772548a684c38dfec49bb8c198ce350cc24e1717
Evelyn Mitchell <efm-github@linsomniac.com>
2013-11-08 22:39:54 -0700

    initial commit

---
+++ BacktestOverfitting/BHYmethod.R
@@ -0,0 +1,6 @@
+# BMY backtest overitting test
+# port of matlab code from 
+#  http://papers.ssrn.com/sol3/papers.cfm?abstract_id=2345489
+#  https://faculty.fuqua.duke.edu/~charvey/backtesting/
+# 
+# data
